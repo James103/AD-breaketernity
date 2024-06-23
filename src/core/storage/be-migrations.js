ae546d11c03dc6299a886e81f1f8cb89863b727b
James103 <37945304+James103@users.noreply.github.com>
2024-06-23 09:53:47 -0500

    Some fixes to allow importing pre-Reality saves
    Fixed some "implicit conversion from Decimal to Number" errors in the following:
    * The big crunch button
    * The progress checker, during the "Late Eternity" stage
    * During migration of the emoji galaxy count
    * During migration of challenge times
    * During the determination of the current tutorial state (in migrations)
    * During migration of purchased Time Theorems
    * In the calculation of Infinity Points
    
    Fixed the Automation tab always having the notification
    
    Decimalized the repeatable ×2 IP upgrade

--- src/core/storage/be-migrations.js
+++ src/core/storage/be-migrations.js
@@ -238,6 +238,7 @@ export function beMigration(player) {
   player.replicanti.timer = D(player.replicanti.timer);
   player.requirementChecks.reality.slowestBH = D(player.requirementChecks.reality.slowestBH);
   player.requirementChecks.reality.maxID1 = D(player.requirementChecks.reality.maxID1);
+  player.requirementChecks.permanent.emojiGalaxies = D(player.requirementChecks.permanent.emojiGalaxies);
   player.totalTickGained = D(player.totalTickGained);
   player.totalTickBought = D(player.totalTickBought);
 }
